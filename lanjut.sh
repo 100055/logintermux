bf2e48eee94562aa3123322bfe25ac83d5be501b
Azzar(NoName) <33390030+100055@users.noreply.github.com>
2018-11-09 19:24:55 +0700

    Delete lanjut.sh

--- lanjut.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Selamat Datang"
-read -p "Masukkan Untuk Username Mu: " user
-
-echo "#!/bin/bash\ntrap ctrl_c INT 2\nctrl_c(){\necho 'Exiting'\nsleep 2\nexit\n}\np=$user\nread -p 'Masukkan Username: ' nama\nclear\nif [ "$"nama = "$"p ]\nthen\necho '\033[92m<-----Selamat Datang----->'\ncat $PREFIX/etc/motd\nsleep 1\nbash\nelse\necho 'Login Gagal'\nexit 1\nfi">log
-
